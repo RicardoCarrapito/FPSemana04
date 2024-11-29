0d398b626224bdb7d671b4721b72e51cc31b400e
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2024-11-29 10:44:03 +0000

    first commit

---
+++ read_json.py
@@ -0,0 +1,14 @@
+import json
+try:
+    caminho=input()
+    file= open(caminho,"rt",encoding="utf8")
+    data= file.read()
+
+    data= json.loads(data)
+    print(data)
+    file.close()
+
+except Exception:
+    print("Ocorreu um erro!")
+finally:
+    print("Processo Concluido!")
